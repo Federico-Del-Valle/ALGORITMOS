168e6ddba9031528aa0835e2370120a0f03483c6
Federico Del Valle <fedexdelvalle@gmail.com>
2025-06-07 17:54:29 -0300

    Ejercicio parada de colectivo

---
+++ Ejercicios/EjercicioParadaColectivos/EjercicioParadaColectivo/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int paradas[10] = {1,2,3,4,5,6,7,8,9,10};
+    int i ;
+    int opcion ;
+    printf("--Inicio programa\n --");
+
+    for(i = 0; i< 10; i++)
+    {
+        printf("\n parada nro %d", paradas[i]);
+        printf("\nIngrese la opcion: ")
+        printf("\n 1- sube pasajero");
+        printf("\n ")
+        scanf("%d", opcion);
+
+    }
+    return 0;
+}
---
+++ Ejercicios/EjercicioParadaColectivos/EjercicioParadaColectivo/pasajero.h
@@ -0,0 +1,12 @@
+#ifndef PASAJERO_H_INCLUDED
+#define PASAJERO_H_INCLUDED
+
+typedef struct
+{
+    char nombre[31];
+    int paradaAs;
+    int paradaDes;
+}pasajero;
+
+
+#endif // PASAJERO_H_INCLUDED
